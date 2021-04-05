e475bc160dfd28465e700e6a62cff1cb38aa2e9a
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-05 01:17:15 -0400

    unique string arr

--- arrays.js
+++ arrays.js
@@ -28,7 +28,60 @@ console.log(ReverseStr2("Hi, does this works?"));
 // Merge Sorted Arrays
 // [0,3,5,10] [1,2,7,15]
 
-const mergeSortedArrays = (a, b) =>
-  [...a, ...b].sort((c, d) => c-d);
+const mergeSortedArrays = (a, b) => [...a, ...b].sort((c, d) => c - d);
 
 console.log(mergeSortedArrays([0, 3, 5, 57], [1, 2, 50]));
+
+// determine if a string has all unique characters
+//abcde gives true
+//aabbc gives false
+
+// not very efficient, o(n^2)
+function uniqueChar(str) {
+  str = str.toString().toLowerCase(); //converts to lowercase
+
+  let trueVal;
+  let falseVal;
+
+  //makes sure the string is not undefined and is a string
+  if (!str || typeof str !== "string") {
+    return "Enter a valid string";
+  }
+
+  //if string length is 1, it is unique
+  if (str.length === 1) {
+    return "Yep, it is unique";
+  }
+
+  //conversts string to an array
+  const strArr = [...str];
+
+  //loops over every element
+  for (let i = 0; i < strArr.length; i++) {
+    const element = strArr[i];
+
+    //result is a new array of all filtered character
+    const result = strArr.filter((char) => char === element);
+
+    //if more that one character, it is not unique else unique
+    if (result.length > 1) {
+      console.log("false");
+      falseVal = true;
+      break;
+    } else {
+      trueVal = true;
+      console.log("true");
+    }
+  }
+
+  if (falseVal) {
+    return false;
+  }
+
+  if (trueVal) {
+    return true;
+  }
+}
+
+uniqueChar(13123123);
+uniqueChar("false");
