fad18e45e16b1409e1eafa66a3a3e0243676778a
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-05 16:47:40 -0400

    shift working

--- linkedList.js
+++ linkedList.js
@@ -53,14 +53,48 @@ class LinkedList {
       this.head = null;
       this.tail = null;
     }
+    return temp;
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    // If list is empty, return undefined
+    if (!this.head) {
+      return undefined;
+    }
+    // If list has two or more nodes
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    // If list has one node, this.length-- above makes length 0,
+    // but the node doesn't go away. this.head is null above.
+    // Thus the code below.
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
+
+  get(index) {
+      
   }
 }
 
-const myLinkedList = new LinkedList(4);
+const myLinkedList = new LinkedList(5);
 
-myLinkedList.push(7);
-myLinkedList.push(15);
-
-myLinkedList.pop();
+myLinkedList.unshift(5454646);
 
 console.log(myLinkedList);
