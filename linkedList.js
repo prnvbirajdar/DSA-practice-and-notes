5fe6093f8e62f426fdbabfc73aab109cea9f7bd7
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-05 15:55:51 -0400

    pop working

--- linkedList.js
+++ linkedList.js
@@ -30,8 +30,29 @@ class LinkedList {
     return this;
   }
 
-  pop(value){
-      
+  pop() {
+    // If list is empty, return undefined
+    if (!this.head) {
+      return undefined;
+    }
+
+    // If list has two or more nodes
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+
+    // If list has one node, this.length-- above makes length 0,
+    // but the node doesn't go away. Thus the code below.
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
   }
 }
 
@@ -40,5 +61,6 @@ const myLinkedList = new LinkedList(4);
 myLinkedList.push(7);
 myLinkedList.push(15);
 
+myLinkedList.pop();
 
 console.log(myLinkedList);
