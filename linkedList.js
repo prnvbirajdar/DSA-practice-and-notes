82c94d587de9ca589a21979683ed10daa51d356f
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-05 15:23:52 -0400

    linked list push working

--- linkedList.js
+++ linkedList.js
@@ -16,8 +16,29 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop(value){
+      
+  }
 }
 
 const myLinkedList = new LinkedList(4);
+
+myLinkedList.push(7);
+myLinkedList.push(15);
+
 
 console.log(myLinkedList);
