234c4692bbda9ae7e6c07e10731e9063a769e6f7
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-06 21:40:39 -0400

    BST insert

--- trees.js
+++ trees.js
@@ -13,7 +13,47 @@ class BST {
     this.root = null;
   }
 
- 
+  insert(value) {
+    // create new node
+    const newNode = new Node(value);
+
+    // condition 1: BST is empty
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    // we declare a variable and compare it to root and every subsquent parent nodes
+    // while traversing the tree
+    let temp = this.root;
+
+    // always going to true unless it hits a return statement
+    while (true) {
+      // makes sure the new Node does not match already existing nodes
+      if (newNode.value === temp.value) return undefined;
+      // if new node value is less than root value / temp value
+      if (newNode.value < temp.value) {
+        // if left side is empty, assign it with new node
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        // else assign temp a new value as the left value and loop through again
+        temp = temp.left;
+      }
+      // if new node value is greater than root value / temp value
+      else {
+        // if right side is empty, assign it with new node
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        // else assign temp a new value as the right value and loop through again
+        temp = temp.right;
+      }
+    }
+  }
+
 }
 
 const myTree = new BST();
