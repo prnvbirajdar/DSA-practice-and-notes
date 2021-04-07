a6504472187db5a6f16192f748a3afd5719fbceb
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-06 21:40:21 -0400

    BST contructor

---
+++ trees.js
@@ -0,0 +1,27 @@
+// New Node Class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// BST Class
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+ 
+}
+
+const myTree = new BST();
+
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+
+myTree.insert(52);
+myTree.insert(82);
