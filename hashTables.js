737c52bf7c2785c4cc05b05746b16a9ebfbc4448
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-07 20:27:19 -0400

    HT set

--- hashTables.js
+++ hashTables.js
@@ -7,16 +7,34 @@ class HashTable {
     this.dataMap = new Array(size);
   }
   // hash function
-  // underscore is to denote not to called methos directly and 
+  // underscore is to denote not to called methos directly and
   // that it should be only be called by other methods
+  // key is the key string
   _hash(key) {
     let hash = 0;
+    // it gives a number between 0 and 6 for each other
+    // 23 is used because it is prime number
     for (let i = 0; i < key.length; i++) {
       hash = (hash + key.charCodeAt(i) * 23) % this.dataMap.length;
     }
+    // always gives values between 0 and 6
     return hash;
   }
+
+  set(key, value) {
+    // index to figure out which array position to push our key-value pair
+    let index = this._hash(key);
+    // if nothing exists in index, we create a new empty array
+    if (!this.dataMap[index]) this.dataMap[index] = [];
+    // we then push they key-value pairs
+    this.dataMap[index].push([key, value]);
+    return this;
+  }
 }
+
+let myHashTable = new HashTable();
+
+console.log(myHashTable);
 
 // function that return first recurring number i.e. 2
 
@@ -33,5 +51,3 @@ function recurringNum(arr) {
 
 array1 = [2, 3, 4, 5, 6, 3, 2, 4, 5];
 array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
-
-console.log(recurringNum(array2));
