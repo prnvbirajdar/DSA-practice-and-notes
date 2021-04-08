b214104f4568d57c10792cf48d98a35dc9bcbd7e
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-08 00:11:59 -0400

    Graph addEdge

--- graph.js
+++ graph.js
@@ -12,7 +12,16 @@ class Graph {
     return false;
   }
 
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex2].push(vertex1);
+      this.adjacencyList[vertex1].push(vertex2);
+      return true;
+    }
+    return false;
+  }
 
+ 
 }
 
 let myGraph = new Graph();
