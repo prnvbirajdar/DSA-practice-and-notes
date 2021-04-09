a7c3d4cc87604bd2dbbcd07c69be70a35f39d8d2
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-09 00:30:26 -0400

    bubble sort understood

--- Algo/bubbleSort.js
+++ Algo/bubbleSort.js
@@ -3,8 +3,11 @@ function bubbleSort(arr) {
 
   // n-1 because we loop n-1 times through the array
   for (let i = 0; i < n - 1; i++) {
+    console.log(arr[i]);
     // loops again, this time only looping through (n-i-1) items
     for (let j = 0; j < n - i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+
       if (arr[j] > arr[j + 1]) {
         // swaps the items
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
@@ -14,4 +17,23 @@ function bubbleSort(arr) {
   return arr;
 }
 
-//arr1 = [8, 6, 4, 7, 5, 1, 2, 3];
+//arr1 = [5,3,2,1,4];
+
+//COLT STEELE video explains it really well
+//this is his method
+
+function bubbleSort2(arr) {
+  let n = arr.length;
+
+  for (let i = n; i > 0; i--) {
+    console.log(arr[i]);
+
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
