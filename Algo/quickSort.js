f3743dcdd8736255e45f6faf2ce312404f663b36
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-09 15:33:03 -0400

    pivot function

--- Algo/quickSort.js
+++ Algo/quickSort.js
@@ -1,4 +1,19 @@
 
+// watch the video for pivot code explanation
+function pivotFunc(array, pivotIndex = 0, endIndex = array.length - 1) {
+  let swapIndex = pivotIndex;
+
+  for (let i = pivotIndex + 1; i <= endIndex; i++) {
+    if (array[i] < array[pivotIndex]) {
+      swapIndex++;
+      swap(array, swapIndex, i);
+    }
+  }
+  swap(array, pivotIndex, swapIndex);
+
+  return swapIndex;
+}
+
 function swap(array, firstIndex, secondIndex) {
   [array[firstIndex], array[secondIndex]] = [
     array[secondIndex],
