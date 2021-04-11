5e82172e1326d6688dc553a37ec336fd31fed01b
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-11 19:47:18 -0400

    dfs post-order

--- Algo/Search/bfs.js
+++ Algo/Search/bfs.js
@@ -80,6 +80,21 @@ class BST {
     traverse(this.root);
     return results;
   }
+
+  postOrderDFS() {
+    let results = [];
+
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+      results.push(currentNode.value);
+    }
+
+    traverse(this.root);
+    return results;
+  }
+
+ 
 }
 
 let myTree = new BST();
