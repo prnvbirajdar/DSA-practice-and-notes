abc1a45edec9452c9a176846c262d9391e281015
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-11 19:01:54 -0400

    dfs pre-order

--- Algo/Search/bfs.js
+++ Algo/Search/bfs.js
@@ -67,6 +67,19 @@ class BST {
 
     return results;
   }
+
+  preOrderDFS() {
+    let results = [];
+
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
