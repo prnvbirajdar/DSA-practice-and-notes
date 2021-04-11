511daf9015c77c5aed5280b3e26a17e0fdb3effd
prnvbirajdar <prnvbirajdar@gmail.com>
2021-04-11 19:47:32 -0400

    dfs in-order

--- Algo/Search/bfs.js
+++ Algo/Search/bfs.js
@@ -94,7 +94,18 @@ class BST {
     return results;
   }
 
- 
+  inOrderDFS() {
+    let results = [];
+
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
